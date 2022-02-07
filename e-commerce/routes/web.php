3b09896c88675d356c231d54906c42cfd3dc30a7
MoniReach0902 <monireach090200@gmail.com>
2022-02-07 22:48:32 +0700

    update

--- e-commerce/routes/web.php
+++ e-commerce/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Backend\AdminProfileController;
 use Illuminate\Support\Facades\Route;
 use Laravel\Jetstream\Rules\Role;
 use Illuminate\Support\Facades\Auth;
@@ -26,9 +27,12 @@ Route::group(['prefix'=>'admin','middleware'=>['admin:admin']],function(){
 Route::middleware(['auth:sanctum,admin', 'verified'])->get('/admin/dashboard', function () {
     return view('admin.index');
 })->name('dashboard');
-// Admin Route
-Route::get('/admin/logout', [AdminController::class, 'destroy'])->name('admin.logout');
-
 Route::middleware(['auth:sanctum,web', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+
+// Admin Route
+Route::get('/admin/logout', [AdminController::class, 'destroy'])->name('admin.logout');
+Route::get('/admin/profile', [AdminProfileController::class, 'adminProfile'])->name('admin.profile');
+Route::get('/admin/profile/edit', [AdminProfileController::class, 'adminProfileEdit'])->name('admin.profile.edit');
